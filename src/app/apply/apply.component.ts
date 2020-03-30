aa6bf1a8e9e93f7033dc4586f207c179354148f6
pinnocd <dean_pinnock@yahoo.com>
2020-03-30 22:42:48 +0100

    Neaten application form and set default data

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -76,7 +76,22 @@ export class ApplyComponent {
     'Parish or town council'
   ];
 
-  constructor() { }
+  constructor() { 
+    // Set up default sample data
+    this.Org_model.name = "Fredrick Pollock Organisation";
+    this.Org_model.address = "43 Alperce Street, Mansfield, Yorkshire";
+    this.Org_model.postcode = "NF5 6GF";
+    this.Org_model.email = "fill.ball@grep.com";
+    this.Org_model.website = "https://www.dailydot.com/parsec/mcu-movies-order-marvel-cinematic-universe-timeline/";
+    this.Org_model.type = "School";
+    this.Org_model.charity = true;
+    this.Org_model.charity_no = 6702932;
+    this.Org_model.start_date = new Date("1998-12-22"); //new Date("2019-01-16");  //"23-JAN-1998";
+    this.Org_model.open = false;
+    this.Org_model.info = "The three-hour conclusion to the Infinity Saga (and the direct sequel to Infinity War) delivered the end that we expected: the surviving Avengers reversed Thanos’ snap, bringing back the MCU’s fallen heroes and half the universe. But before it gets there, Avengers: Endgame took its time to allow those who remained to grieve, leading to an improbable time heist to grab the Infinity Stones from the past—and visit previous MCU films. But Thanos’ final defeat doesn’t come without sacrifices and reflection. Black Widow sacrificed herself on Vormir, Iron Man sacrificed himself to deliver the final blow to Thanos, Thor passed on his crown to Valkyrie, and Captain America stayed in the past after returning the Infinity Stones and grew old before passing on his shield to Sam Wilson. —Michelle Jaworski";
+
+    this.Gen_model.problem = "kjehrkjherbfkjhbrf";
+  }
 
   onSubmit(form) {
     console.log(form.value)
