3760da5da593af23065cc27b9cc883cb8ba1d817
pinnocd <dean_pinnock@yahoo.com>
2020-04-14 13:10:28 +0100

    check for nowt

--- ngnf-php/config/database.php
+++ ngnf-php/config/database.php
@@ -5,11 +5,6 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=UTF-8");
 
-define('DB_HOST', 'ngnf-mysql.cp842gttjsxj.eu-west-2.rds.amazonaws.com');
-define('DB_USER', 'dbuser');
-define('DB_PASS', 'Gone5w4zfrQhMPoVZ5Dv');
-define('DB_NAME', 'ngnf');
-define('DB_PORT', '3306');
 
 function connect()
 {
