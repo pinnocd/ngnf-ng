01846ecd9362dc485ed1120f37f83b994b3733aa
pinnocd <dean_pinnock@yahoo.com>
2020-06-22 22:47:53 +0100

    point php server to cloudfront

--- globals.ts
+++ globals.ts
@@ -17,9 +17,9 @@ export class Globals {
 
 
 export const    PHP_API_SERVER = 
-//                    "https://d3en7m4sk97aoa.cloudfront.net";
+                    "https://d3en7m4sk97aoa.cloudfront.net";
 //                  "http://ec2-35-177-242-73.eu-west-2.compute.amazonaws.com";
-                  "http://localhost:8080";
+//                  "http://localhost:8080";
 
 export var      username:   string;
 
